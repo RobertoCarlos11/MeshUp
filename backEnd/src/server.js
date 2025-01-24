cdfdbbe3b28f2a018f701feb6b37f38752593352
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-01-24 16:39:07 -0600

    created server file running on port 5000

---
+++ backEnd/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js" 
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)); 
