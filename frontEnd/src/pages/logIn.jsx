1b51cae7f851a09f0a78be6b6f519852fd70ada0
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-01-24 16:40:28 -0600

    Added example functionality on login Page

--- frontEnd/src/pages/logIn.jsx
+++ frontEnd/src/pages/logIn.jsx
@@ -1,23 +1,50 @@
 import { useState } from "react";
-
-
+import { getUsers,userLogIn } from "../services/userService";
+import { useNavigate } from "react-router-dom";
 
 function logIn(){
 
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState([]);
     const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
-    const handleForm = (e) => {
+    
+    const handleForm = async (e) => {
         e.preventDefault();
-        alert(user);
-        alert(password);
+        const userFound = await userLogIn(user, password);
+        if(userFound.data)
+        {
+            alert("GG SI EXISTE");
+            navigate("/Home");
+        }
+        else{
+            alert("GG no existe");
+        }
+    }   
+
+    const handleUserButton = async () => {
+        const allUsers = await getUsers();
+        setUserData(allUsers.data[0]);
     }
 
     return(
+        <>
         <form onSubmit={handleForm} className="">
             <input className="" type="text" onChange={(e) => {setUser(e.currentTarget.value)}} name="username" id="username"/>
             <input type="password" onChange={(e) => {setPassword(e.currentTarget.value)}} name="password" id="password"/>
             <button type="submit">Enviar </button>
         </form>
+
+        <button onClick={handleUserButton}>E dale click we</button>
+        
+        {userData && (
+            userData.map((user,index) => (
+            <div key={index}>
+                <p>{user.Username}</p>
+            </div>
+            ))
+        )}
+        </>
     )
 }
 
