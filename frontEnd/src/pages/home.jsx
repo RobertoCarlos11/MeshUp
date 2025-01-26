3b60ab554eee62d44207ca9e6e74dd6107e9557d
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-01-25 21:36:27 -0600

    added scene component to home for example of the scene

--- frontEnd/src/pages/home.jsx
+++ frontEnd/src/pages/home.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import Header from "../components/header"
+import Scene from "../components/Three/scene"
 function Home()
 {
+    const [modelUrl, setModelUrl] = useState(null);
+    const [textureUrl, setTextureUrl] = useState(null);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if(file)
+        {
+            const objecturl = URL.createObjectURL(file);
+            setModelUrl(objecturl);
+        }
+    }
+
+    const handleTextureFile = (e) => {
+        const file = e.target.files[0];
+        if(file)
+        {
+            const objecturl = URL.createObjectURL(file);
+            setTextureUrl(objecturl);
+        }
+    }
     return (
         <>
+        <Header />
         <h1>Hola bienvenido a la pagina principal MESH UP!!!</h1>
+        <div className="flex flex-col space-y-5 items-center">
+            <input type="file" onChange={handleFileChange} id="modelFile" />
+            <input type="file" onChange={handleTextureFile} id="textureFile" />
+        </div>
+        <Scene modelUrl = {modelUrl} textureUrl={textureUrl}/>
         </>
     )
 }
