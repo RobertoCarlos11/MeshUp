d7bab054f14a411bf841aaa7e2f459aef7551b9f
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-05 17:36:52 -0600

    Created full login functionality

--- frontEnd/src/pages/logIn.jsx
+++ frontEnd/src/pages/logIn.jsx
@@ -1,52 +1,39 @@
 import { useState } from "react";
 import Header from "../components/Header"   
 import { getUsers,userLogIn } from "../services/userService";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function logIn(){
 
     const navigate = useNavigate();
-    const [userData, setUserData] = useState([]);
     const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
-    
-    const handleForm = async (e) => {
+
+    const handleLoginButton = async (e) => {
         e.preventDefault();
-        const userFound = await userLogIn(user, password);
-        if(userFound.data)
-        {
-            alert("GG SI EXISTE");
-            navigate("/Home");
-        }
-        else{
-            alert("GG no existe");
-        }
-    }   
+        const userFound = await userLogIn(user,password);
+        console.log(userFound);
 
-    const handleUserButton = async () => {
-        const allUsers = await getUsers("Dobeto", "Dobeto123");
-        setUserData(allUsers.data);
+        alert(userFound.status ? `Usuario Encontrado: ${userFound.data.Username}` : "No existe o t equivocaste pendejo");
+
     }
-
     return(
-        <>
-        <Header />
-        <form onSubmit={handleForm} className="">
-            <input className="" type="text" onChange={(e) => {setUser(e.currentTarget.value)}} name="username" id="username"/>
-            <input type="password" onChange={(e) => {setPassword(e.currentTarget.value)}} name="password" id="password"/>
-            <button type="submit">Enviar </button>
-        </form>
-
-        <button onClick={handleUserButton}>E dale click we</button>
-        
-        {userData && (
-            userData.map((user,index) => (
-            <div key={index}>
-                <p>{user.Username}</p>
+        <div className="h-screen w-screen flex justify-center items-center space-x-auto">
+            <div className="bg-secondary px-12 py-16 rounded-md">
+                <h1 className="text-comp-1 text-center font-bold text-2xl p-8">Welcome Back!</h1>
+                <h2 className="text-comp-1 text-center font-bold text-xl">Log In</h2>
+                <form className="flex flex-col space-y-6 p-6">
+                    <input className="text-xs text-comp-1 border-b-1 border-[var(--primary-color)]" onChange={(e) => setUser(e.target.value)} type="text" placeholder="Username or Email Address"/>
+                    <input className="text-xs text-comp-1 border-b-1 border-[var(--primary-color)]" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"/>
+                    <div className="flex justify-center">
+                    <button onClick={handleLoginButton} className="w-1/2 text-xs bg-primary text-comp-1 font-semibold rounded-sm p-1">Log In</button>
+                    </div>
+                </form>
+                <div className="flex justify-center">
+                <Link className="text-comp-1 font-light text-xs text-center">No account?. No problem!</Link>
+                </div>
             </div>
-            ))
-        )}
-        </>
+        </div>
     )
 }
 
