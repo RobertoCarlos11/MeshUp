cd798020f7041599516e20b68c3e29452d7e3ffa
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-06 14:16:26 -0600

    Added Rout to post page

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import LogIn from './pages/LogIn'
 import Home from './pages/Home'
+import Post from './pages/Post'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LogIn />} />
           <Route path="/Home" element={<Home />} />
+          <Route path="/Post" element={<Post />} />
         </Routes>
       </Router>
   )
