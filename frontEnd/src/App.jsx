e336a3672ce29785650c3e3136b33567caecca31
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-06 17:32:32 -0600

    Created and added Upload page to router

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import LogIn from './pages/LogIn'
 import Home from './pages/Home'
 import Post from './pages/Post'
+import Upload from './pages/Upload'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<LogIn />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Post" element={<Post />} />
+          <Route path="/Upload" element={<Upload />} />
         </Routes>
       </Router>
   )
