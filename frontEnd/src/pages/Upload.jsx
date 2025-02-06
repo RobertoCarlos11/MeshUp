9ab2071a98e1eaade7aca1d3bd2c2a50519c215f
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-06 17:44:23 -0600

    Added transition of colors to button

--- frontEnd/src/pages/Upload.jsx
+++ frontEnd/src/pages/Upload.jsx
@@ -33,12 +33,12 @@ function Upload() {
                 <Scene className="h-1/2" modelUrl={model && URL.createObjectURL(model)} textureUrl = {texture && URL.createObjectURL(texture)}/>
                 <div className="flex w-full justify-between">
                     <div className="flex w-1/2 justify-between text-primary">
-                        <button onClick={handleUploadModel} className="flex items-center">
+                        <button onClick={handleUploadModel} className="flex p-1 rounded-md items-center hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] transition duration-150 ease-in-out">
                             <ViewInArIcon />
                             <p className="text-xs">{model ? model.name : "Add a model..."}</p>
                             <input type="file" ref={modelRef} onChange={handleModelChange} hidden />
                         </button>
-                        <button onClick={handleUploadTexture} className="flex items-center">
+                        <button onClick={handleUploadTexture} className="flex p-1 rounded-md items-center hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] transition duration-150 ease-in-out">
                             <AddPhotoAlternateIcon />
                             <p className="text-xs">{texture ? texture.name : "Add a texture..."}</p>
                             <input type="file" ref={textureRef} onChange={handleTextureChange} hidden />
