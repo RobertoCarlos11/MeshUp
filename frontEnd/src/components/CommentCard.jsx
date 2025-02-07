4b460cdc01b0521ff4ed117815dcdeb56202e0a1
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-06 18:00:08 -0600

    Added props for future backend implementation

--- frontEnd/src/components/CommentCard.jsx
+++ frontEnd/src/components/CommentCard.jsx
@@ -1,6 +1,5 @@
 import React from "../assets/react.svg"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
 import Rating from "../components/Rating"
 
 function CommentCard(props) {
@@ -9,19 +8,19 @@ function CommentCard(props) {
             <div className="flex justify-between">
                 <div className="flex space-x-2">
                     <img src={React} className="" />
-                    <h1 className="text-primary text-md font-bold flex items-center">User</h1>
+                    <h1 className="text-primary text-md font-bold flex items-center">{props.User}</h1>
                 </div>
-                <Rating className="text-yellow-400" stars={3}/>
+                <Rating className="text-yellow-400" stars={props.Rating}/>
             </div>
             <div>
-                <p className="text-comp-1 text-md">This is a review</p>
+                <p className="text-comp-1 text-md">{props.Review}</p>
                 <div className="flex justify-between">
                     <div className="flex space-x-1 text-xs">
                         <FavoriteBorderIcon className="text-primary" />
-                        <p className="text-comp-1 flex items-center">523 Likes</p>
+                        <p className="text-comp-1 flex items-center">{props.Likes} Likes</p>
                     </div>
                     <div className="text-xs text-secondary">
-                    <p>01/02/2025 - 7:30 PM</p>
+                    <p>{props.Date}</p>
                     </div>
                 </div>
             </div>
@@ -29,4 +28,11 @@ function CommentCard(props) {
     )
 }
 
+CommentCard.defaultProps = {
+    User: "Dobeto",
+    Rating: 5,
+    Review: "Wow!, what a nice model you did :D",
+    Likes: 52,
+    Date: "04/02/2025 - 7:30 PM",
+};
 export default CommentCard;
