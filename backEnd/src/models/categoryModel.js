3b89b726d39daa2c357b31fb388a48962a1a544f
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-08 16:58:59 -0600

    Created category Model mapping from DB

---
+++ backEnd/src/models/categoryModel.js
@@ -0,0 +1,28 @@
+import { Description } from "@mui/icons-material";
+import sequelize from "../config/db";
+import { DataTypes } from "sequelize";
+
+const Category = sequelize.define(
+    "Category",
+    {
+        CategoryId:{
+            type: DataTypes.SMALLINT(),
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        Category_Name:{
+            type: DataTypes.STRING(30),
+            allowNull:true,
+        },
+        Category_Description:{
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        }
+    },
+    {
+        tableName:"Category",
+        timestamps: false
+    }
+);
+
+export default Category;
