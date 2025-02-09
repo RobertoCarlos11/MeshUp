360e3652b67320a35f8d0228cdb25c53624f1e3f
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-09 16:51:31 -0600

    Added link to navigate to home when clicking the logo

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import LogoName from "../assets/LogoName.png";
 import DefaultPfp from "../assets/no-user.png";
 import HeaderComponent from "./HeaderComponent";
-
+import {Link} from "react-router-dom";
 function Header(){
     return(
     <>
         <nav className="flex flex-row justify-center items-center p-3 text-xl">
-            <img src={LogoName} alt="LogoName" className="h-10 w-auto m-2"/> |
+            <Link to="/Home" className="w-1/8 m-2">
+            <img src={LogoName} alt="LogoName"/>
+            </Link>
             <input type="text" placeholder="Search" className="w-400 text-base text-opacity-10 border-2 border-solid border-[var(--primary-color)] rounded-sm m-2 p-2 pb-1 pt-1"/> |
             <HeaderComponent/>
             <img src={DefaultPfp} alt="ProfilePic" className="h-10 w-auto m-2 rounded-full"/>
