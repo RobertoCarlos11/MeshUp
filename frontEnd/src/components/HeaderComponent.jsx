ac768eac9361047ac062f9dcb12599acb41faaa0
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-10 14:42:28 -0600

    Cursor Pointer to buttons + commented buttons

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -6,11 +6,11 @@ function HeaderComponent(){
     return(
     <>
         {/* Active session */}
-        <InsertChartOutlinedOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
-        <NotificationsOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
-        <FileUploadOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
-        <button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Log In</button>
-        <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Sign In</button>
+        <InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+        <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+        <FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+        {/* <button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Log In</button>
+        <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Sign In</button> */}
     </>
     )
 }
