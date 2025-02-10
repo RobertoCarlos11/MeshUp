dd28837354dc7cb9eff758b54dde0239d221323d
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-10 15:41:25 -0600

    Search Bar icon

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,15 +1,19 @@
 import LogoName from "../assets/LogoName.png";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import DefaultPfp from "../assets/no-user.png";
 import HeaderComponent from "./HeaderComponent";
 
 function Header(){
     return(
     <>
-        <nav className="flex flex-row justify-center items-center p-3 text-xl">
+        <nav className="cursor-pointer flex flex-row justify-between items-center p-3 text-xl ml-5 mr-5">
             <img src={LogoName} alt="LogoName" className="h-10 w-auto m-2"/> |
-            <input type="text" placeholder="Search" className="w-400 text-base text-opacity-10 border-2 border-solid border-[var(--primary-color)] rounded-sm m-2 p-2 pb-1 pt-1"/> |
+            <div class="relative m-2">
+                <SearchOutlinedIcon className="absolute transform left-2 translate-y-1 text-[var(--primary-color)]"/>
+                <input type="text" placeholder="Search" className="w-380 text-sm text-opacity-10 border-2 border-solid border-[var(--primary-color)] rounded-sm p-1 pl-9"/>
+            </div> |
             <HeaderComponent/>
-            <img src={DefaultPfp} alt="ProfilePic" className="h-10 w-auto m-2 rounded-full"/>
+            <img src={DefaultPfp} alt="ProfilePic" className="cursor-pointer h-10 w-auto m-2 rounded-full"/>
         </nav>
     </>
     )
