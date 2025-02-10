0a89a8368beb56a45d81d2c38cd13d68ad719725
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-10 12:18:23 -0600

    added camera positioning to the center of the model

--- frontEnd/src/components/Three/scene.jsx
+++ frontEnd/src/components/Three/scene.jsx
@@ -1,18 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
+import { useState } from "react";
 
 const Scene = (props) => 
 {
+    const [cameraPosition, setCameraPosition] = useState([0,0,50]);
+
     return(
         <div className={props.className}>
-            <Canvas className="rounded-md" style={{width:"100%" }}>
+            <Canvas className="rounded-md" style={{width:"100%" }} camera={{position: cameraPosition}}>
                 <color attach="background" args={["lightgray"]} />  
                 <ambientLight intensity={0.5}/>
                 <directionalLight position={[2,5,3]}/>
                 <OrbitControls/>
-                {props.modelUrl && <Model modelUrl={props.modelUrl} textureUrl={props.textureUrl}/>}
+                {props.modelUrl && <Model modelUrl={props.modelUrl} textureUrl={props.textureUrl} setCameraPosition={setCameraPosition}/>}
             </Canvas>
+            {props.children};
         </div>
     )
 }
@@ -22,4 +26,6 @@ Scene.defaultProps = {
     width: " ",
     height: " ",
 }
+
+
 export default Scene
