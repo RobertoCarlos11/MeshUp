bb381c43b1e1d981fd55fa76b8b26323c60d7975
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-10 14:44:26 -0600

    Added "Profile" route

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import LogIn from './pages/LogIn'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Post from './pages/Post'
 import Upload from './pages/Upload'
+import Profile from './pages/Profile'
 import './App.css'
 
 function App() {
@@ -13,9 +14,10 @@ function App() {
           <Route path="/Home" element={<Home />} />
           <Route path="/Post" element={<Post />} />
           <Route path="/Upload" element={<Upload />} />
+          <Route path="/Profile" element={<Profile />} />
         </Routes>
       </Router>
   )
 }
 
-export default App
+export default App;
