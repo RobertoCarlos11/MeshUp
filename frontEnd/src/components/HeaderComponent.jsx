a68f2123847e125c0ff06245a960c7dcce353bec
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-10 20:36:19 -0600

    added Navigation to LogIn for the header component

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -1,7 +1,7 @@
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutlinedOutlined';
-
+import { Link } from 'react-router-dom';
 function HeaderComponent(){
     return(
     <>
@@ -9,8 +9,14 @@ function HeaderComponent(){
         <InsertChartOutlinedOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
         <NotificationsOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
         <FileUploadOutlinedIcon className='m-2 text-[var(--primary-color)]'/>
-        <button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Log In</button>
-        <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Sign In</button>
+        <div className='space-x-1 flex w-60'>
+        <Link className='w-30' to="/">
+        <button className='text-base cursor-pointer bg-[var(--primary-color)] w-full rounded-sm p-2'>Log In</button>
+        </Link>
+        <Link className='w-30' to="/">
+        <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-full rounded-sm p-2'>Sign In</button>
+        </Link>
+        </div>
     </>
     )
 }
