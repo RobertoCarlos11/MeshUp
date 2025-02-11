d9c2934f23d22619d9ded8e9bea919e10fbeb321
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-11 12:38:36 -0600

    Modified center header component elements & button desing

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -5,12 +5,13 @@ import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutl
 function HeaderComponent(){
     return(
     <>
-        {/* Active session */}
-        <InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
-        <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
-        <FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
-        {/* <button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Log In</button>
-        <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-2'>Sign In</button> */}
+        <div className='flex justify-center'>
+            <InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+            <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+            <FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
+            <button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Log In</button>
+            <button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Sign In</button>  
+        </div>
     </>
     )
 }
