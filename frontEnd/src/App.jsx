a07c507b96f7ce8c779093dd5a5890e48fefd848
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-10 21:09:44 -0600

    Added "Search_History" route

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home'
 import Post from './pages/Post'
 import Upload from './pages/Upload'
 import Profile from './pages/Profile'
+import Search_History from './pages/Search_History'
 import './App.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/Post" element={<Post />} />
           <Route path="/Upload" element={<Upload />} />
           <Route path="/Profile" element={<Profile />} />
+          <Route path="/Search_History" element={<Search_History />} />
         </Routes>
       </Router>
   )
