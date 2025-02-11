659a040024a2ef9c811031c28e769a4ac86ef180
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-11 11:52:10 -0600

    Added "Collection" route

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,6 +5,7 @@ import Post from './pages/Post'
 import Upload from './pages/Upload'
 import Profile from './pages/Profile'
 import Search_History from './pages/Search_History'
+import Collection from './pages/Collection'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/Upload" element={<Upload />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Search_History" element={<Search_History />} />
+          <Route path="/Collection" element={<Collection />} />
         </Routes>
       </Router>
   )
