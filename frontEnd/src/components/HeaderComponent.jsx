368be53ed0e6c3e0e9bc232723e9f49bd814a2a6
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-11 16:11:34 -0600

    Modified classnames

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -9,8 +9,8 @@ function HeaderComponent(){
             <InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
             <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
             <Link to="/Upload"><FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/></Link>
-            <Link className='w-30' to="/"><button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Log In</button></Link>
-            <Link className='w-30' to="/"><button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Sign In</button></Link> 
+            <Link to="/"><button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Log In</button></Link>
+            <Link to="/"><button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Sign In</button></Link> 
         </div>
     </>
     )
