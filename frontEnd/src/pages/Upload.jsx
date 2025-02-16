e7486ea7ca832c8303876dffdd190ed915e0e6a1
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-15 20:41:07 -0600

    Added Button_Style to Upload + changed layout and desing details

--- frontEnd/src/pages/Upload.jsx
+++ frontEnd/src/pages/Upload.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import Scene from "../components/Three/Scene";
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import Button_Style from "../components/Button_Style";
 import { useRef, useState } from "react";
 
 function Upload() {
@@ -33,23 +34,23 @@ function Upload() {
         <Header/>
         <div className="space-x-auto h-screen flex justify-center">
             <div className="w-1/2 flex flex-col space-y-4">
-                <input type="text" placeholder="Add Title..." className="text-comp-1 font-bold text-2xl w-full" />
+                <input type="text" placeholder="Add Title..." className="text-comp-1 font-bold text-2xl w-full border-b-2 border-solid border-[var(--primary-color)]" />
                 <Scene className="h-1/2" modelUrl={model && URL.createObjectURL(model)} textureUrl = {texture && URL.createObjectURL(texture)}/>
                 <div className="flex w-full justify-between">
-                    <div className="flex w-1/2 justify-between text-primary">
-                        <button onClick={handleUploadModel} className="flex p-1 rounded-md items-center hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] transition duration-150 ease-in-out">
+                    <div className="flex flex-row">
+                        <button onClick={handleUploadModel} className="cursor-pointer rounded-md items-center border-1 border-solid border-[var(--primary-color)] hover:bg-[var(--primary-color)] active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] m-1 p-3 pt-1 pb-1 transition duration-150 ease-in-out">
                             <ViewInArIcon />
-                            <p className="text-xs">{model ? model.name : "Add a model..."}</p>
+                            <span className="text-xs">{model ? model.name : "Add a model..."}</span>
                             <input type="file" ref={modelRef} onChange={handleModelChange} hidden />
                         </button>
-                        <button onClick={handleUploadTexture} className="flex p-1 rounded-md items-center hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] transition duration-150 ease-in-out">
+                        <button onClick={handleUploadTexture} className="cursor-pointer rounded-md items-center border-1 border-solid border-[var(--primary-color)] hover:bg-[var(--primary-color)] active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] m-1 p-3 pt-1 pb-1 transition duration-150 ease-in-out">
                             <AddPhotoAlternateIcon />
-                            <p className="text-xs">{texture ? texture.name : "Add a texture..."}</p>
+                            <span className="text-xs">{texture ? texture.name : "Add a texture..."}</span>
                             <input type="file" ref={textureRef} onChange={handleTextureChange} hidden />
 
                         </button>
                     </div>
-                    <select className="w-1/3 bg-primary t96ext-comp-1 font-semibold rounded-md text-comp-1">
+                    <select className="w-1/3 text-sm bg-[var(--background-color)] border-1 border-solid border-[var(--primary-color)] rounded-sm p-1 m-1">
                         <option value="Animals">Animals</option>
                         <option value="Technology">Technology</option>
                         <option value="Youtube">Youtube</option>
@@ -58,9 +59,11 @@ function Upload() {
                 </div>
                 <div className="space-y-1">
                     <h2 className="text-xl font-bold text-comp-1">Description:</h2>
-                    <textarea placeholder="Add a description..." className="w-full h-40 rounded-md border-1 border-[var(--primary-color)] text-comp-1 text-xs"></textarea>
+                    <textarea placeholder="Add a description..." className="w-full h-40 rounded-sm border-2 border-[var(--primary-color)] text-xs p-2"></textarea>
                 </div>
-                <button className="bg-primary text-comp-1 rounded-md w-1/2 self-center">Upload</button>
+                
+                <center><Button_Style name="Upload" className="text-sm m-2 p-3 pt-1 pb-1 w-1/3"/></center>
+                
             </div>
         </div>
         <Footer/>
