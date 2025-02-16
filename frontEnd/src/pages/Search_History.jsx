602f34b2af1c5523b3a80d9cf136494dd7ed6643
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-15 20:40:05 -0600

    Added Button_Style to Search_History

--- frontEnd/src/pages/Search_History.jsx
+++ frontEnd/src/pages/Search_History.jsx
@@ -2,6 +2,8 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Day_History from "../components/Day_History";
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import Button_Style from "../components/Button_Style";
+import Button_Style2 from "../components/Button_Style2";
 
 function Search_History(){
     return(
@@ -10,19 +12,19 @@ function Search_History(){
                 <div className="space-x-auto ml-100 mr-100">
                     <div className="flex flex-row justify-between border-b-2 border-solid p-2">
                         <div className="flex items-center m-2">
-                            <CloseOutlinedIcon className="cursor-pointer ml-2 mr-2"/>
-                            <span className="text-base font-semibold ml-2 mr-2">00</span>
-                            <span className="text-base ml-2 mr-2">Selected elements</span>
+                            <CloseOutlinedIcon className="cursor-pointer m-2"/>
+                            <span className="text-base font-semibold m-2">00</span>
+                            <span className="text-base m-2">Selected elements</span>
                         </div>
-                        <button className="border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm p-3 pb-1 pt-1">Delete</button>
+                        <Button_Style2 name="Delete" className="text-sm m-2 p-3 pb-1 pt-1"/>
                     </div>
 
                     <Day_History/>
                     <Day_History/>
                     <Day_History/> 
 
-                    <center><button className="justify-center bg-[var(--primary-color)] cursor-pointer rounded-sm m-5 p-3 pb-2 pt-2">Delete Search History</button></center>
-                    
+                    <center>
+                        <Button_Style name="Delete Search History" className="text-sm m-5 p-3 pb-1 pt-1 w-1/3"/></center>
                 </div>
             <Footer/>
         </>
