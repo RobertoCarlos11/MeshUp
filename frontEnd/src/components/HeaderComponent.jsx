692621063d77e8ad763d626395b1373045b03c1b
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-15 20:38:12 -0600

    Added Button_Style to Header

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -1,7 +1,10 @@
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutlinedOutlined';
+import Button_Style from "../components/Button_Style";
+import Button_Style2 from "../components/Button_Style2";
 import { Link } from 'react-router-dom';
+
 function HeaderComponent(){
     return(
     <>
@@ -9,8 +12,8 @@ function HeaderComponent(){
             <Link to="/Reports"><InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/></Link>
             <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
             <Link to="/Upload"><FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/></Link>
-            <Link to="/"><button className='text-base cursor-pointer bg-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Log In</button></Link>
-            <Link to="/"><button className='text-base cursor-pointer border-2 border-solid border-[var(--primary-color)] w-30 rounded-sm m-2 p-3 pb-1 pt-1'>Sign In</button></Link> 
+            <Link to="/"><Button_Style name="Log In" className="text-base w-30 m-2 p-3 pb-1 pt-1"/></Link>
+            <Link to="/"><Button_Style2 name="Sign In" className="text-base w-30 m-2 p-3 pb-1 pt-1"/></Link> 
         </div>
     </>
     )
