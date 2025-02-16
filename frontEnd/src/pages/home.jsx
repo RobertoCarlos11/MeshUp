e7d2b1331030b06cdc26a7c57286cb1d5faf65c1
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-02-15 20:38:34 -0600

    Added Button_Style to Home

--- frontEnd/src/pages/home.jsx
+++ frontEnd/src/pages/home.jsx
@@ -2,6 +2,8 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import PostCard from "../components/PostCard";
 import Pagination from "../components/Pagination";
+import Button_Style from "../components/Button_Style";
+import Button_Style2 from "../components/Button_Style2";
 
 function Home(){
     
@@ -9,8 +11,8 @@ function Home(){
         <div className="">
             <Header/>
             <div className="flex text-sm space-x-auto mr-5 ml-5">
-                <button className="bg-[var(--primary-color)] cursor-pointer rounded-sm m-2 p-3 pb-1 pt-1">Category</button>
-                <button className="border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm m-2 p-3 pb-1 pt-1">Category</button>
+                <Button_Style name="Category" className="m-2 p-3 pb-1 pt-1"/>
+                <Button_Style2 name="Category" className="m-2 p-3 pb-1 pt-1"/>
             </div>
             <div className="flex flex-wrap justify-center space-x-auto m-5">
                 <PostCard/>
