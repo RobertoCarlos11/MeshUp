440c70f6a575a384416a6bb930cc84c6186c5202
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-17 16:47:18 -0600

    added inverted property to button style for both style of buttons

--- frontEnd/src/components/Button_Style.jsx
+++ frontEnd/src/components/Button_Style.jsx
@@ -1,8 +1,13 @@
-function Button_Style({ children, className }) {
-    return (
-        <button className={`bg-[var(--primary-color)] border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-transparent active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
-            {children}
-        </button>
-    );
+function Button_Style({ children = "", className = "", inverted = false }) {
+    
+    return inverted ? 
+        (<button className={`border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-[var(--primary-color)] active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
+        {children}
+        </button>)
+        :
+        (<button className={`bg-[var(--primary-color)] border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-transparent active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
+        {children}
+        </button>)
 }
+
 export default Button_Style;
