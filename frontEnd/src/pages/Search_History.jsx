76eda9c7af0358731f44bc7876f37b0c89a83ded
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-17 15:32:14 -0600

    Removed name attribute from button Style and replaced with children property

--- frontEnd/src/pages/Search_History.jsx
+++ frontEnd/src/pages/Search_History.jsx
@@ -5,28 +5,29 @@ import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import Button_Style from "../components/Button_Style";
 import Button_Style2 from "../components/Button_Style2";
 
-function Search_History(){
-    return(
+function Search_History() {
+    return (
         <>
-            <Header/>
-                <div className="space-x-auto ml-100 mr-100">
-                    <div className="flex flex-row justify-between border-b-2 border-solid p-2">
-                        <div className="flex items-center m-2">
-                            <CloseOutlinedIcon className="cursor-pointer m-2"/>
-                            <span className="text-base font-semibold m-2">00</span>
-                            <span className="text-base m-2">Selected elements</span>
-                        </div>
-                        <Button_Style2 name="Delete" className="text-sm m-2 p-3 pb-1 pt-1"/>
+            <Header />
+            <div className="space-x-auto ml-100 mr-100">
+                <div className="flex flex-row justify-between border-b-2 border-solid p-2">
+                    <div className="flex items-center m-2">
+                        <CloseOutlinedIcon className="cursor-pointer m-2" />
+                        <span className="text-base font-semibold m-2">00</span>
+                        <span className="text-base m-2">Selected elements</span>
                     </div>
-
-                    <Day_History/>
-                    <Day_History/>
-                    <Day_History/> 
-
-                    <center>
-                        <Button_Style name="Delete Search History" className="text-sm m-5 p-3 pb-1 pt-1 w-1/3"/></center>
+                    <Button_Style2 name="Delete" className="text-sm m-2 p-3 pb-1 pt-1" />
                 </div>
-            <Footer/>
+
+                <Day_History />
+                <Day_History />
+                <Day_History />
+
+                <Button_Style className="text-sm m-5 p-3 pb-1 pt-1 w-1/3">
+                    Delete Search History
+                </Button_Style>
+            </div>
+            <Footer />
         </>
     )
 }
