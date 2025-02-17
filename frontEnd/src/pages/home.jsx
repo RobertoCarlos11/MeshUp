1835367db32aca183cb22665c7794f916bb2b9bc
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-17 15:30:23 -0600

    removed name property from the button style and button style 2

--- frontEnd/src/pages/home.jsx
+++ frontEnd/src/pages/home.jsx
@@ -11,8 +11,8 @@ function Home(){
         <div className="">
             <Header/>
             <div className="flex text-sm space-x-auto mr-5 ml-5">
-                <Button_Style name="Category" className="m-2 p-3 pb-1 pt-1"/>
-                <Button_Style2 name="Category" className="m-2 p-3 pb-1 pt-1"/>
+                <Button_Style className="m-2 p-3 pb-1 pt-1" > Category </Button_Style>
+                <Button_Style2 name="Category" className="m-2 p-3 pb-1 pt-1"> Category </Button_Style2>
             </div>
             <div className="flex flex-wrap justify-center space-x-auto m-5">
                 <PostCard/>
