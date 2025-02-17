10fa05350cb5eeb452ade08389006466ba7e6e75
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-17 16:26:27 -0600

    Created SearchHistoryModel for sequelize

---
+++ backEnd/src/models/SearchHistoryModel.js
@@ -0,0 +1,40 @@
+import sequelize from "../config/db";
+import { DataTypes } from "sequelize";
+
+const SearchHistory = sequelize.define(
+    "SearchHistory",{
+        HistoryId:{
+            type: DataTypes.SMALLINT(),
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        Search:{
+            type: DataTypes.STRING(50),
+            allowNull:false,
+        },
+        Search_Date:{
+            type: DataTypes.DATE(),
+            allowNull:true,
+        },
+        Searh_Status:{
+            type: DataTypes.BOOLEAN(),
+            defaultValue: true,
+        },
+        Email:{
+            type: DataTypes.STRING(50),
+            allowNull:false,
+            references:{
+                model:"User",
+                key:"Email",
+            }
+        }
+    },
+    {
+        tableName:"Search_History",
+        timestamps: false
+    }
+);
+
+SearchHistory.associate = (models) => {
+    SearchHistory.belongsTo(models.User,{foreignKey:"Email", as: "users"});
+};
