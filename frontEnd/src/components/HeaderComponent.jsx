5690088818d5fad1cc1d9378022ecae948302b33
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-17 15:31:19 -0600

    Removed unnecesary name attribute to button Style and button style 2

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -12,8 +12,8 @@ function HeaderComponent(){
             <Link to="/Reports"><InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/></Link>
             <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/>
             <Link to="/Upload"><FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]'/></Link>
-            <Link to="/"><Button_Style name="Log In" className="text-base w-30 m-2 p-3 pb-1 pt-1"/></Link>
-            <Link to="/"><Button_Style2 name="Sign In" className="text-base w-30 m-2 p-3 pb-1 pt-1"/></Link> 
+            <Link to="/"><Button_Style className="text-base w-30 m-2 p-3 pb-1 pt-1">Log In</Button_Style></Link>
+            <Link to="/"><Button_Style2 name="Sign In" className="text-base w-30 m-2 p-3 pb-1 pt-1"> Sign In </Button_Style2></Link> 
         </div>
     </>
     )
