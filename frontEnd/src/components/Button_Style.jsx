0fdffcef5f3524b9a8bfcaf522348e7adf59d986
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-19 16:00:27 -0600

    Added onClick prop to the button that is rendered

--- frontEnd/src/components/Button_Style.jsx
+++ frontEnd/src/components/Button_Style.jsx
@@ -1,11 +1,11 @@
-function Button_Style({ children = "", className = "", inverted = false }) {
+function Button_Style({ children = "", className = "", inverted = false, onClick }) {
     
     return inverted ? 
-        (<button className={`border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-[var(--primary-color)] active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
+        (<button onClick={onClick} className={`border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-[var(--primary-color)] active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
         {children}
         </button>)
         :
-        (<button className={`bg-[var(--primary-color)] border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-transparent active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
+        (<button onClick={onClick} className={`bg-[var(--primary-color)] border-2 border-solid border-[var(--primary-color)] cursor-pointer rounded-sm hover:bg-transparent active:bg-[var(--secondary-color)] active:border-[var(--secondary-color)] transition duration-150 ease-in-out ${className}`}>
         {children}
         </button>)
 }
