429e6e82da20f32341fe4d9f73be0e7250fae0f2
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-19 16:01:00 -0600

    added Post route for userRegister

--- backEnd/src/routes/userRoutes.js
+++ backEnd/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getAllUsers, userLogIn } from "../controllers/userController.js";
+import { getAllUsers, userLogIn,userRegister } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/",getAllUsers);
 router.get("/:user/:password", userLogIn);
+router.post("/:user/:password/:birthdate/:email", userRegister);
 
 export default router;
