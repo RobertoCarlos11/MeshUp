52c2e639f532da308291a4c02e03b4b2a08ee8f7
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-23 17:43:12 -0600

    added category,Model,and Post routes to the server

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js" 
+import categoryRoutes from "./routes/categoryRoutes.js" 
+import ModelRoutes from "./routes/3DModelRoutes.js" 
+import PostRoutes from "./routes/PostRoutes.js" 
 import cors from "cors";
 import models from "./config/db.js";
 
@@ -12,6 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/3DModel", ModelRoutes);
+app.use("/api/post", PostRoutes);
 
 const PORT = process.env.PORT || 5000;
 
