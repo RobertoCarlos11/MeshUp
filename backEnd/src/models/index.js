4f188d058c16b2d5573618ff2800262a78b2b924
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-24 16:19:40 -0600

    created manual joins of the models for sequelize

--- backEnd/src/models/index.js
+++ backEnd/src/models/index.js
@@ -1,27 +1,57 @@
-import { modalClasses } from "@mui/material";
-import sequelize from "../config/db";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import sequelize from "../config/db.js";
+import User from "./UserModel.js";
+import Post from "./PostModel.js";
+import Model from "./3DFileModel.js";
+import Category from "./CategoryModel.js";
+import Comment from "./CommentModel.js";
+import Collection from "./CollectionModel.js";
+import SearchHistory from "./SearchHistoryModel.js";
+import Notification from "./NotificationModel.js";
+import CollectionElement from "./CollectionElementModel.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+User.hasMany(Post, {foreignKey:"Email", as:"posts"});
+Post.belongsTo(User,{foreignKey:"Email", as:"user"});
 
-const models = {};
+User.hasMany(Comment, {foreignKey:"Email", as:"comments"});
+Comment.belongsTo(User, {foreignKey:"Email", as:"user"});
 
-fs.readdirSync(__dirname)
-.filter((file) => file !== "index.js")
-.forEach((file) => {
-    const model = require(path.join(__dirname, file)).default;
-    models[models.name] = model;
-});
+User.hasMany(Collection, {foreignKey:"Email", as:"collections"});
+Collection.belongsTo(User,{foreignKey:"Email", as:"user"});
 
-Object.keys(models).forEach((modelName) => {
-    if(models[modelName].associate)
-        models[modelName].associate(models);
-});
+User.hasMany(SearchHistory, {foreignKey:"Email", as:"history"});
+SearchHistory.belongsTo(User,{foreignKey:"Email", as:"user"});
 
-models.sequelize = sequelize;
-models.Sequelize = sequelize.Sequelize;
+User.hasMany(Notification, {foreignKey:"Emitter_Email", as:"notifications"});
+Notification.belongsTo(Notification,{foreignKey:"Emitter_Email", as:"emitter"});
 
-export default models;
+
+Post.belongsTo(Model,{foreignKey:"ModelId", as:"model"});
+Model.hasOne(Post,{foreignKey:"ModelId", as:"posts"});
+
+Post.belongsTo(Category,{foreignKey:"CategoryId", as:"category"});
+Category.hasMany(Post, {foreignKey:"CategoryId",as:"posts"});
+
+Post.hasMany(Comment,{foreignKey:"PostId", as:"comments"});
+Comment.belongsTo(Post, {foreignKey:"PostId", as:"posts"});
+
+Post.hasMany(Notification,{foreignKey:"PostId", as:"posts"});
+Notification.belongsTo(Post,{foreignKey:"PostId", as:"notifications"});
+
+Collection.hasMany(CollectionElement, { foreignKey: "CollectionId", as: "elements" });
+CollectionElement.belongsTo(Collection, { foreignKey: "CollectionId", as: "collection" });
+
+Post.hasMany(CollectionElement, { foreignKey: "PostId", as: "collectionElements" });
+CollectionElement.belongsTo(Post, { foreignKey: "PostId", as: "post" });
+
+const syncDB = async () => 
+{
+  try {
+    await sequelize.sync({alter:false});
+    console.log("Database Synchronized");
+  }catch(error)
+  {
+    console.error("Error syncing database", error);
+  }
+};
+
+export {syncDB,sequelize};
