c1cded7d2450cdf6383698d62398de6b6a9197c9
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-28 14:51:31 -0600

    Added edit profile functionality using multer for uploading the profile picture

--- backEnd/src/controllers/userController.js
+++ backEnd/src/controllers/userController.js
@@ -1,11 +1,13 @@
 import { where, Op } from 'sequelize';
 import User from '../models/UserModel.js';
-import fs from 'fs';
+import multer from "multer";
 
-export const getAllUsers = async (req,res) => {
-    try{
+const upload = multer({ storage: multer.memoryStorage() });
+
+export const getAllUsers = async (req, res) => {
+    try {
         const users = await User.findAll();
-        
+
         const payload = {
             status: true,
             message: "Users fetched correctly",
@@ -13,26 +15,26 @@ export const getAllUsers = async (req,res) => {
         };
         res.json(payload);
     }
-    catch(error)
-    {
+    catch (error) {
         res.status(500).json(error);
     }
 }
 
 export const userLogIn = async (req, res) => {
-    try{
-        const {user, password} = req.params;
+    try {
+        const { user, password } = req.params;
         const userFound = await User.findOne({
             where:
             {
-                [Op.or]:[
-                    {Username: user},
-                    {Email: user}
+                [Op.or]: [
+                    { Username: user },
+                    { Email: user }
                 ],
                 Pass: password,
-            }}
+            }
+        }
         );
-        
+
         const payload = {
             status: userFound ? true : false,
             message: userFound ? "User Found" : "User Not Found",
@@ -40,23 +42,21 @@ export const userLogIn = async (req, res) => {
         }
         res.json(payload);
     }
-    catch(error)
-    {
+    catch (error) {
         res.status(500).json(error);
         console.log(error);
     }
 }
 
-export const userRegister = async (req,res) => {
-    try
-    {
-        const {user, password,birthdate,email} = req.body;
+export const userRegister = async (req, res) => {
+    try {
+        const { user, password, birthdate, email } = req.body;
         await User.create({
             Username: user,
             Pass: password,
             Email: email,
             Birthdate: birthdate,
-            Profile_Picture: null 
+            Profile_Picture: null
         });
 
         const payload = {
@@ -65,9 +65,44 @@ export const userRegister = async (req,res) => {
         };
         res.json(payload);
     }
+    catch (error) {
+        res.status(500).json(error);
+        console.log(error);
+    }
+}
+
+export const userUpdate = async (req, res) => {
+    try {
+        const { Username,Pass,Birthdate,Email,Profile_Picture } = req.body;
+
+        let ProfilePictureBuffer = null;
+        if(!Profile_Picture)
+        {
+            ProfilePictureBuffer = req.files["Profile_Picture"][0].buffer;
+        }
+
+        await User.update(
+            {
+                Username: Username,
+                Pass: Pass,
+                Birthdate: Birthdate,
+                Profile_Picture: ProfilePictureBuffer === null ? {} : ProfilePictureBuffer,
+            },
+            {
+                where: { Email: Email },
+            }
+        );
+
+
+        const payload = {
+            status: true,
+            message: "User updated successfully",
+        };
+        res.json(payload);
+    }
     catch(error)
     {
         res.status(500).json(error);
-        console.log(error);   
+        console.log(error);
     }
 }
--- frontEnd/src/pages/Profile.jsx
+++ frontEnd/src/pages/Profile.jsx
@@ -3,16 +3,36 @@ import Footer from "../components/Footer";
 import Profile_Sections from "../components/Profile_Sections";
 import DefaultPfp from "../assets/no-user.png";
 import Button_Style from "../components/Button_Style";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Modal } from "@mui/material";
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import { updateUser } from "../services/userService";
+import Swal from "sweetalert2";
 
 function Profile() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-
+    const [iconHidden, setIconHidden] = useState(true);
+    const [photoUrl, setPhotoUrl] = useState(null);
     const [open, setOpen] = useState(false);
-
+    const [userUpdated, setUserUpdated] = useState(true);
+    const [photoArray, setPhotoArray] = useState([]);    
+    const PhotoInputRef = useRef();
+    const PhotoRef = useRef();
+    
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    useEffect(() => {
+        if(!userUpdated)
+            return;
+
+        localStorage.setItem("user", JSON.stringify(user));
+        const PhotoArray = new Uint8Array(user.Profile_Picture.data);
+        const PhotoBlob = new Blob([PhotoArray], {type:"image/png"});
+        const PhotoUrl = URL.createObjectURL(PhotoBlob);
+        setPhotoUrl(PhotoUrl);
+        setUserUpdated(false);
+    },[userUpdated, user]);
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
@@ -22,14 +42,75 @@ function Profile() {
         return `${year}-${month}-${day}`;
     };
 
+    const openPhotoFile = () => {
+        PhotoInputRef.current.click();
+    }
+
+    const handlePhotoChange = (e) => {
+        const photoSelected = e.currentTarget.files[0];
+
+        setUser((prev) =>({
+            ...prev,
+            Profile_Picture: photoSelected,
+        }));
+        if(photoSelected)
+            {
+                const reader = new FileReader();
+                reader.readAsArrayBuffer(photoSelected);
+                
+                reader.onload = () => {
+                    const uint8Array = new Uint8Array(reader.result); 
+                    setPhotoArray([...uint8Array]);
+                    PhotoRef.current.src = URL.createObjectURL(new Blob([reader.result],{type: photoSelected.type}));
+                };
+        }
+    }
+
+    const handleSave = async (e) => 
+    {
+        e.preventDefault();
+        const formData = new FormData();
+
+        for(const key in user)
+        {
+            if(user.hasOwnProperty(key))
+                formData.append(key, user[key]);
+        }
+        const response = await updateUser(formData);
+
+        if(response.status)
+        {
+            handleClose();
+            Swal.fire({
+            title:"Successfully updated!",
+            text:"Your information was successfully updated",
+            icon:"success",
+            timer:2000,});
+        }   
+        setUser((prev) => ({
+            ...prev,
+            Profile_Picture: {data: photoArray.length === 0 ? prev.Profile_Picture.data : photoArray,type:"buffer",},
+        }));
+        
+        setUserUpdated(true);
+    }
+
+    const handleUpdateUser = (e) => 
+    {
+        const {name,value} = e.target;
+        setUser((prev) => ({
+                ...prev,
+            [name]: value,
+        }));
+    }
     return (
         <>
-            <Header />
+            <Header UserUpdated={userUpdated}/>
             <div className="bg-[var(--secondary-color)] h-40"></div>
 
             <div className="flex flex-col justify-center">
                 <div className="flex justify-center transform -translate-y-1/2">
-                    <img src={DefaultPfp} alt="ProfilePicture" className="border-4 border-solid border-[var(--background-color)] rounded-full w-45 h-45" />
+                    <img src={photoUrl ?? DefaultPfp} alt="ProfilePicture" className="border-4 border-solid border-[var(--background-color)] rounded-full w-45 h-45" />
                 </div>
                 <div className="flex flex-col -mt-18 mb-3">
                     <span className="text-center text-2xl font-semibold">{user.Username}</span>
@@ -53,32 +134,36 @@ function Profile() {
                         <div className="">
                             <div className="mb-4">
                                 <label className="block text-sm font-bold mb-2">Username</label>
-                                <input type="text" defaultValue={user.Username} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                <input type="text" name="Username" onChange={handleUpdateUser} defaultValue={user.Username} className="shadow appearance-none border rounded w-full py-2 px-3 text-comp-1 leading-tight focus:outline-none focus:shadow-outline" />
                             </div>
                             <div className="mb-4">
                                 <label className="block text-sm font-bold mb-2">Password</label>
-                                <input type="text" defaultValue={user.Pass} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                <input type="text" name="Pass" onChange={handleUpdateUser} defaultValue={user.Pass} className="shadow appearance-none border rounded w-full py-2 px-3 text-comp-1 leading-tight focus:outline-none focus:shadow-outline" />
                             </div>
                             <div className="mb-4"></div>
                             <label className="block text-sm font-bold mb-2">Birthdate</label>
-                            <input type="date" defaultValue={formatDate(user.Birthdate)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                            <input type="date" name="Birthdate" onChange={handleUpdateUser} defaultValue={formatDate(user.Birthdate)} className="shadow appearance-none border rounded w-full py-2 px-3 text-comp-1 leading-tight focus:outline-none focus:shadow-outline" />
                         </div>
                         <div className="mb-4 flex flex-col items-center">
                             <label className="block text-sm font-bold mb-2">Profile Picture</label>
-                            <input type="file" hidden />
-                            <img src={user.Profile_Picture === null ? user.Profile_Picture : DefaultPfp} className="w-50" />
+                            <input type="file" hidden ref={PhotoInputRef} onChange={(e) => handlePhotoChange(e)}/>
+                            <div onMouseEnter={() => setIconHidden(false)} onMouseLeave={() => setIconHidden(true)} className="relative w-50">
+                                <div onClick={() => openPhotoFile()} hidden={iconHidden} className="absolute inset-0 bg-black/50"></div>
+                                <img ref={PhotoRef} src={photoUrl} className="z-0 w-50" />
+                                <AddAPhotoIcon className="absolute inset-0 m-auto w-12 h-12 text-white" hidden={iconHidden} />
+                            </div>
                         </div>
                         <div className="flex items-center justify-between">
                         </div>
                     </form>
-                        <div className="flex justify-evenly">
-                            <Button_Style className="text-sm px-3 py-1" onClick={handleClose}>
-                                Save
-                            </Button_Style>
-                            <Button_Style className="text-sm px-3 py-1" onClick={handleClose}>
-                                Cancel
-                            </Button_Style>
-                        </div>
+                    <div className="flex justify-evenly">
+                        <Button_Style className="text-sm px-3 py-1" onClick={handleSave}>
+                            Save
+                        </Button_Style>
+                        <Button_Style className="text-sm px-3 py-1" onClick={handleClose}>
+                            Cancel
+                        </Button_Style>
+                    </div>
                 </div>
             </Modal >
             <Profile_Sections />
