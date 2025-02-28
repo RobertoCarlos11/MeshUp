355365a25791a0b57993a8300063121a2b0b555b
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-28 14:51:03 -0600

    added categorySelected style whenever you click a category

--- frontEnd/src/pages/home.jsx
+++ frontEnd/src/pages/home.jsx
@@ -5,7 +5,7 @@ import Pagination from "../components/Pagination";
 import Button_Style from "../components/Button_Style";
 import { useEffect, useState } from "react";
 import { GetAllCategories } from "../services/categoryService";
-import { GetAllPosts } from "../services/PostService";
+import { GetAllPosts } from "../services/postService";
 import Logo from "../assets/Logo.png";
 
 
@@ -14,35 +14,49 @@ function Home(){
     const [categoriesFound, setCategoriesFound] = useState();
     const [postsFound, setPostsFound] = useState();
     const [search, setSearch] = useState("");
-
+    const [categorySelected,setCategorySelected] = useState(0);
+    
     const FetchCategories = async () => 
     {
         const categories = await GetAllCategories();
-        console.log(categories);
          setCategoriesFound(categories.data);
     }
     
-    const FetchPosts = async () => {
-        const posts = await GetAllPosts();
+    const FetchPosts = async (categoryId) => {
+        setPostsFound(null);
+        const posts = await GetAllPosts(categoryId);
         setPostsFound(posts.data);
     }
     useEffect(() => {        
-
         FetchCategories();
-        FetchPosts();
-
     },[]);
+
+    useEffect(() => {
+        FetchPosts(categorySelected);
+    },[categorySelected]);
 
     const handleSearch = (word) => 
     {
         setSearch(word);
     }
+
+    const handleCategoryChange = (CategoryId) => 
+    {
+        setCategorySelected(prevCategory => prevCategory === CategoryId ? 0 : CategoryId);
+    }
+    
     return (
         <div className="">
             <Header SearchChanged={handleSearch}/>
             <div className="flex text-sm space-x-auto mr-5 ml-5">
                 {categoriesFound && categoriesFound.map((category) => (
-                        <Button_Style key={category.CategoryId} className="m-2 p-3 pb-1 pt-1" >{category.Category_Name} </Button_Style>
+                        <Button_Style 
+                        onClick={() => handleCategoryChange(category.CategoryId)}
+                        key={category.CategoryId} 
+                        className="m-2 p-3 pb-1 pt-1"
+                        inverted={categorySelected === category.CategoryId}
+                        >{category.Category_Name} 
+                        </Button_Style>
             ))}
             </div>
             <div className="flex flex-wrap justify-center space-x-auto m-5">
