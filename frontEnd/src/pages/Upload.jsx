4a5ca3d6879573d1b0a330f14cd1c330d0f3809e
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-28 14:53:17 -0600

    updated prop name in scene

--- frontEnd/src/pages/Upload.jsx
+++ frontEnd/src/pages/Upload.jsx
@@ -119,7 +119,7 @@ function Upload() {
             <div className="space-x-auto h-screen flex justify-center">
                 <div className="w-1/2 flex flex-col space-y-4">
                     <input type="text" name="title" placeholder="Add Title..." onChange={handlePostChange} className="text-comp-1 font-bold text-2xl w-full border-b-2 border-solid border-[var(--primary-color)]" />
-                    <Scene className="h-1/2" modelUrl={postInfo.model && URL.createObjectURL(postInfo.model)} textureUrl={postInfo.texture && URL.createObjectURL(postInfo.texture)} />
+                    <Scene className="h-1/2" model={postInfo.model && URL.createObjectURL(postInfo.model)} texture={postInfo.texture && URL.createObjectURL(postInfo.texture)} />
                     <div className="flex w-full justify-between">
                         <div className="flex flex-row">
                             <Button_Style inverted onClick={handleUploadModel} className="cursor-pointer m-1 px-3 py-1">
@@ -135,7 +135,7 @@ function Upload() {
                             </Button_Style>
                         </div>
 
-                        <select onChange={handlePostChange} className="w-1/3 text-sm bg-[var(--background-color)] border-1 border-solid border-[var(--primary-color)] rounded-sm p-1 m-1">
+                        <select name="categorySelected" onChange={handlePostChange} className="w-1/3 text-sm bg-[var(--background-color)] border-1 border-solid border-[var(--primary-color)] rounded-sm p-1 m-1">
                             {categoriesFound && categoriesFound.map((category) => (
 
                                 <option key={category.CategoryId} value={category.CategoryId}>{category.Category_Name}</option>
