707369c99412703288101c31c28a295252ba5574
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-28 14:51:41 -0600

    added put route in userRoutes

--- backEnd/src/routes/userRoutes.js
+++ backEnd/src/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-
-import { getAllUsers, userLogIn,userRegister } from "../controllers/userController.js";
+import multer from "multer";
+import { getAllUsers, userLogIn,userRegister,userUpdate } from "../controllers/userController.js";
 
 const router = express.Router();
+const upload = multer({storage:multer.memoryStorage()});
 
 router.get("/",getAllUsers);
 router.get("/:user/:password", userLogIn);
 router.post("/", userRegister);
+router.put("/",upload.fields([{name:"Profile_Picture"}]), userUpdate);
+
 
 export default router;
