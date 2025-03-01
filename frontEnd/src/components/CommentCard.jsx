0a3a35bdfb84ad06c7514f511c45edbee77c674d
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-02-28 19:04:17 -0600

    added photo rendering of user in commentcard

--- frontEnd/src/components/CommentCard.jsx
+++ frontEnd/src/components/CommentCard.jsx
@@ -1,13 +1,29 @@
 import React from "../assets/react.svg"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Rating from "../components/Rating"
+import { useEffect,useState } from "react";
 
 function CommentCard({comment}) {
+
+    const [photoUrl, setPhotoUrl] = useState(null);
+    useEffect(() => {
+        if(!comment.user.Profile_Picture)
+            return;
+        const PhotoArray = new Uint8Array(comment.user.Profile_Picture.data);
+        const PhotoBlob = new Blob([PhotoArray]);
+        const PhotoUrl = URL.createObjectURL(PhotoBlob);
+        setPhotoUrl(PhotoUrl);
+    },[]);
+
+    useEffect(() => 
+    {
+        return () => URL.revokeObjectURL(photoUrl);
+    },[photoUrl]);
     return (
         <div className="border-1 border-[var(--primary-color)] rounded-md p-2">
             <div className="flex justify-between">
                 <div className="flex space-x-2">
-                    <img src={React} className="" />
+                    <img src={photoUrl === null ? React : photoUrl} className="w-10 h-10" />
                     <h1 className="text-primary text-md font-bold flex items-center">{comment.user.Username}</h1>
                 </div>
                 <Rating className="text-yellow-400" stars={comment.Rating}/>
