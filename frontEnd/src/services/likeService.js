fd97fb3b0b511d3777a7ba29754eb07d053050b3
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-03-01 18:02:01 -0600

    created service for inserting and updating a like in likeServices

--- frontEnd/src/services/likeService.js
+++ frontEnd/src/services/likeService.js
@@ -1,9 +1,9 @@
 import apiClient from "../context/apiClient";
 
-export const GetLikes = async (thingLiked, thingLikedId) => 
+export const GetLikes = async (thingLiked, thingLikedId,email) => 
 {
     try{
-        const response = await apiClient.get(`/api/like/${thingLiked}/${thingLikedId}`);
+        const response = await apiClient.get(`/api/like/${thingLiked}/${thingLikedId}/${email}`);
         
         return response.data;
     }
@@ -12,3 +12,38 @@ export const GetLikes = async (thingLiked, thingLikedId) =>
         throw error.response?.data || error.message;
     }
 } 
+
+export const InsertLike = async (thingLiked, thingLikedId, Email) => 
+{
+    try{
+        const response = await apiClient.post("/api/like",{
+            thingLiked: thingLiked,
+            thingLikedId: thingLikedId,
+            Email: Email,
+        });
+
+        return response.data;
+    }
+    catch(error)
+    {
+        throw error.response?.data || error.message;
+    }
+}
+
+export const UpdateLike = async(thingLiked, thingLikedId, Email, Status) => 
+{
+    try{
+        const response = await apiClient.put("/api/like",{
+            thingLiked: thingLiked,
+            thingLikedId: thingLikedId,
+            Email: Email,
+            status: Status,
+        });
+
+        return response.data;
+    }
+    catch(error)
+    {
+        throw error.response?.data || error.message;
+    }
+}
