901ed3ef2051ec7b8c0a3504b50c0ebdfb217b24
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-02 18:47:30 -0600

    Modified Search History link name

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -61,7 +61,7 @@ function Header({SearchChanged, UserUpdated = true}){
 
                         <PopoverPanel className="absolute right-0 w-40 h-auto bg-[var(--background-color)] shadow-sm border-2 border-solid border-[var(--primary-color)] rounded-sm p-2 z-50 text-xs flex flex-col space-y-2">
                             <Link to="/Profile">See Profile</Link>
-                            <Link  to="/Search_History">History</Link>
+                            <Link  to="/Search_History">Search History</Link>
                             <p className="cursor-pointer" onClick={handleLogOut}>Log Out</p>
                         </PopoverPanel>
                     </Popover> 
