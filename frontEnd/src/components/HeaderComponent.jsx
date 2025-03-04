1014d5987d5179732e8075306104f6d63270f68b
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-04 14:15:29 -0600

    Added redirection to Sign in page

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -1,7 +1,6 @@
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutlinedOutlined';
 import Button_Style from "../components/Button_Style";
-import Button_Style2 from "../components/Button_Style2";
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import Notification from './Notification';
@@ -29,7 +28,7 @@ function HeaderComponent() {
                             <Link to="/">
                                 <Button_Style className="text-base w-30 m-2 p-3 pb-1 pt-1">Log In</Button_Style>
                             </Link>
-                            <Link to="/">
+                            <Link to="/Register">
                                 <Button_Style inverted className="text-base w-30 m-2 p-3 pb-1 pt-1"> Sign In </Button_Style>
                             </Link>
                         </>
