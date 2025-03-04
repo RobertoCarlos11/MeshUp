8e17d1996348e3f88be68142987bd27bbf3e6d03
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-03-03 19:25:13 -0600

    Added Notification component that takes as parameter the user thats logged In

--- frontEnd/src/components/HeaderComponent.jsx
+++ frontEnd/src/components/HeaderComponent.jsx
@@ -1,10 +1,10 @@
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutlinedOutlined';
 import Button_Style from "../components/Button_Style";
 import Button_Style2 from "../components/Button_Style2";
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import Notification from './Notification';
 
 function HeaderComponent() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
@@ -17,7 +17,7 @@ function HeaderComponent() {
                         <Link to="/Reports">
                             <InsertChartOutlinedOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]' />
                         </Link>
-                        <NotificationsOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]' />
+                        <Notification userLoggedIn={user.Email}/>
                         <Link to="/Upload">
                             <FileUploadOutlinedIcon className='cursor-pointer m-2 text-[var(--primary-color)]' />
                         </Link>
