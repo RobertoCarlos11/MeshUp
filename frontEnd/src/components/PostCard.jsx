b94314851f812220c8f584eecfa3763a13810eb2
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-05 00:07:00 -0600

    Added create & add post to collection

--- frontEnd/src/components/PostCard.jsx
+++ frontEnd/src/components/PostCard.jsx
@@ -1,23 +1,38 @@
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
-
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import Rating from './Rating';
 import Scene from './Three/Scene';
+import Button_Style from './Button_Style';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GetLikes,InsertLike,UpdateLike } from '../services/likeService';
 import Like_Button from './Like_Button';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
+import { Modal } from "@mui/material";
 import Swal from 'sweetalert2';
+import { InsertCollection, InsertCollectionElement } from '../services/collectionService';
+import { getCollections } from '../services/collectionService';
 
 function PostCard({ Post }) {
     const navigate = useNavigate();
-
-    const user = JSON.parse(localStorage.getItem("user"));
+    
+    const userLoggedIn = JSON.parse(localStorage.getItem("user"));
+    const [user, setUser] = useState();
     const [modelUrl, setModelUrl] = useState(null);
     const [likes, setLikes] = useState();
     const [userLiked, setUserLiked] = useState();
     const [textureUrl, setTextureUrl] = useState(null);
+
+    const [collectionName, setCollectionName] = useState();
+    const [collectionId, setCollectionId] = useState();
+    const [userCollections, setUserCollections] = useState([]);
     
+    const [open, setOpen] = useState(false);
+    
+    const handleOpen = () => userLoggedIn === null ?  handleNoSession() : setOpen(true);
+    const handleClose = () => setOpen(false);
+
     useEffect(() => {
         const { model } = Post;
         let modelObjectURL, textureObjectURL;
@@ -41,9 +56,9 @@ function PostCard({ Post }) {
 
     useEffect(() => {
         const getLikesOfPost = async () => {
-            const LikesFound = await GetLikes("post", Post.PostId, user.Email);
-            setLikes(LikesFound.data.count);
-            setUserLiked(LikesFound?.UserLiked.Status);
+                const LikesFound = await GetLikes("post", Post.PostId, userLoggedIn.Email);
+                setLikes(LikesFound.data.count);
+                setUserLiked(LikesFound?.UserLiked.Status);
             }
 
         getLikesOfPost();
@@ -52,35 +67,118 @@ function PostCard({ Post }) {
     const handlePostLike = async () => {
         let response;
 
-        if(user === null)
+        if(userLoggedIn === null)
             return Swal.fire({
                 title:"You need to log in.",
                 text:"Please log in to like the post!",
                 icon:"error",
                 timer:2000,
-        });
+            });
         
         if (userLiked === undefined) {
-            response = await InsertLike("post", Post.PostId, user.Email);
+            response = await InsertLike("post", Post.PostId, userLoggedIn.Email);
             setLikes(likes + 1);
             setUserLiked(true);
         }
         else {
-            response = await UpdateLike("post", Post.PostId, user.Email, !userLiked);
+            response = await UpdateLike("post", Post.PostId, userLoggedIn.Email, !userLiked);
             setLikes(userLiked ? likes - 1 : likes + 1);
             setUserLiked(!userLiked);
         }
     }
 
     const postRating = () => {
-
         let rating = Post.comments.reduce((acc, comment) => acc + comment.Rating, 0);
         rating /= Post.comments.length;
         return rating;
     }
+
+    const handleNoSession = () => {
+        Swal.fire({
+            icon: "error",
+            title: "Oops!!",
+            text: "You need to Log or Sign in to get acess to this function!",
+            // html: `
+            //     <Link to="/">
+            //         <Button_Style className="text-base w-30 m-2 p-3 pb-1 pt-1">Log In</Button_Style>
+            //     </Link>
+            //     <Link to="/Register">
+            //         <Button_Style inverted className="text-base w-30 m-2 p-3 pb-1 pt-1"> Sign In </Button_Style>
+            //     </Link>
+            // `
+        })
+    }
+
+    const createCollection = async(e) => {
+        e.preventDefault();
+        setUser(userLoggedIn);
+    
+        if (!collectionName ){
+            handleClose();
+            Swal.fire({
+                icon: "error",
+                title: "Oops!!",
+                text: "Name your collection!!"
+            }).then((result) => {
+                result.isConfirmed ? handleOpen() : '';
+            });
+        }else{
+            handleClose();
+            const response = await InsertCollection(collectionName, userLoggedIn.Email, Post.PostId);
+            console.log(response);
+
+            if(response.status == true){
+                Swal.fire({
+                    icon: "success",
+                    title: "Sucess",
+                    text: "Collection created sucessfully!!"
+                });
+            }else{
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops!!",
+                    text: "Error at creating collection" 
+                })
+            }
+        }
+
+    }
+
+    const addToCollection = async (collectionId) => {
+        setUser(userLoggedIn);
+
+        const response = await InsertCollectionElement(collectionId, Post.PostId);
+        console.log(collectionId, Post.PostId);
+        console.log(response);
+
+        if(response.status == true){
+            Swal.fire({
+                icon: "sucess",
+                title: "Sucess",
+                text: "Sucessfully added to collection!!"
+            });
+        }else{
+            Swal.fire({
+                icon: "error",
+                title: "Oops!!",
+                text: "Error at adding to collection"
+            });
+        }
+
+    }
+    
+    const getUserCollections = async () => {
+        if(userLoggedIn === null){
+            handleNoSession();
+        }else{
+            const collectionsFound = await getCollections(userLoggedIn.Email);
+            setUserCollections(collectionsFound.data);
+        }
+    }
+
     return (
         <>
-            <div className="relative flex flex-col m-2 bg-white shadow-sm rounded-sm w-145">
+            <div className="relative flex flex-col m-2 bg-white shadow-sm rounded-sm w-140">
                 <Scene className="h-75 rounded-sm relative" model={modelUrl} texture={textureUrl}>
                     <Rating stars={postRating} className="absolute top-2 right-2 text-yellow-400" />
                 </ Scene>
@@ -89,15 +187,74 @@ function PostCard({ Post }) {
                         {Post.Post_Name}
                     </span>
                     <div className='flex flex-row text-[var(--secondary-color)]'>
-                        <div className="flex">
+                        <div className="flex items-center">
                             <Like_Button status={userLiked} onClick={handlePostLike} />
                             <p className="text-base m-1">{likes ? likes : 0}</p>
                         </div>
-                        <div className="flex">
-                            <BookmarkBorderIcon className='cursor-pointer' />
+                        <div className="flex items-center">
+                            <BookmarkBorderIcon className='cursor-pointer'/>
                             <p className="text-base m-1">{Post.Saves ? Post.Saves : 0}</p>
                         </div>
-                        <MoreVertOutlinedIcon className='cursor-pointer' />
+                        
+                        <Popover className="relative">
+                            <PopoverButton className='flex items-center my-2'>
+                                <MoreVertOutlinedIcon className='cursor-pointer'/>
+                            </PopoverButton>
+
+                            <PopoverPanel className='absolute left-0 w-50 h-auto bg-[var(--background-color)] shadow-sm border-2 border-solid border-[var(--primary-color)] rounded-sm p-2 z-50 text-xs text-comp-1 flex flex-col space-y-2'>
+                                <div className='border-b-1 border-solid border-[var(--primary-color)]'>
+                                    <AddOutlinedIcon/>
+                                    <button className='cursor-pointer' onClick={handleOpen}>Add to New Collection</button>
+                                </div>
+
+                                <Modal open= {open} onClose= {handleClose} className='flex items-center justify-center'>
+                                    <div className="flex items-center justify-center w-1/3 h-1/2">
+                                        <div className="p-8 bg-color rounded shadow-lg">
+                                            <header className='border-b-2 border-colid border-[var(--primary-color)] p-2'>
+                                                <h2 className='text-center text-lg font-bold'>New Collection</h2>
+                                            </header>
+                                            <form className='flex flex-col space-x-6 py-8 mx-4 text-center'>
+                                                <span className='text-sm font-bold mb-4'>Name your new collection!</span>
+                                                <input onChange={e => setCollectionName(e.currentTarget.value)} type="text" name="NewCollectionName" placeholder="New Collection Name" className='text-comp-1 w-full p-2 py-1 border-b-1 border-solid border-[var(--primary-color)]'/>
+                                            </form>
+                                            <footer className='text-center border-t-2 border-colid border-[var(--primary-color)] p-2'>
+                                                <Button_Style className='text-sm m-2 p-3 py-1' onClick={createCollection}>Save</Button_Style>
+                                                <Button_Style className='text-sm m-2 p-3 py-1' onClick={handleClose}>Cancel</Button_Style>
+                                            </footer>
+                                        </div>
+                                    </div>
+                                </Modal>
+
+                                {userLoggedIn ? (
+                                <Popover className='relative'>
+                                    <PopoverButton className='flex items-center my-2'>
+                                        <div className='border-b-1 border-solid border-[var(--primary-color)]'>
+                                            <BookmarkBorderIcon/>
+                                            <button className='cursor-pointer' onClick={getUserCollections}>Add to Existing Collection</button>
+                                        </div>
+                                    </PopoverButton>
+                                    <PopoverPanel className='absolute left-50 w-50 h-auto bg-[var(--background-color)] shadow-sm border-2 border-solid border-[var(--primary-color)] rounded-sm p-2 z-50 text-xs text-comp-1 flex flex-col space-y-2'>
+                                    {userCollections?.length > 0 ? ( userCollections.map(collection => (
+                                        <button 
+                                            key={collection.CollectionId} 
+                                            onClick={() => {
+                                                setCollectionId(collection.CollectionId); // Set collection ID when clicked
+                                                addToCollection(collection.CollectionId); // Call addToCollection with selected collection ID
+                                            }} 
+                                            className="flex justify-start border-b-1 border-solid border-[var(--primary-color)] cursor-pointer">
+                                            {collection.Collection_Name}
+                                        </button>
+                                    ))) : (<span className='flex justify-start border-b-1 border-solid border-[var(--primary-color)] cursor-default'>No Collections yet</span>)}
+                                    </PopoverPanel>
+                                </Popover>
+                                ) : (
+                                    <div className='border-b-1 border-solid border-[var(--primary-color)]'>
+                                        <BookmarkBorderIcon />
+                                        <button className='cursor-pointer' onClick={handleNoSession}>Add to Existing Collection</button>
+                                    </div>
+                                )}
+                            </PopoverPanel>
+                        </Popover>
                     </div>
                 </div>
             </div>
