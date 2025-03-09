a3dab38556dacf8eebd13d01ce993fd704bd13a1
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-03-09 17:12:59 -0600

    Add search history retrieval and deletion functions in history service

--- frontEnd/src/services/historyService.js
+++ frontEnd/src/services/historyService.js
@@ -1,8 +1,8 @@
 import apiClient from "../context/apiClient";
 
-export const CreateSearch = async (Search, Search_Date,Email) => {
-    try{
-        const response = await apiClient.post("/api/history/",{
+export const CreateSearch = async (Search, Search_Date, Email) => {
+    try {
+        const response = await apiClient.post("/api/history/", {
             Search: Search,
             Search_Date: Search_Date,
             Email: Email,
@@ -10,8 +10,31 @@ export const CreateSearch = async (Search, Search_Date,Email) => {
 
         return response.data;
     }
-    catch(error)
-    {
+    catch (error) {
+        throw error.response?.data || error.message;
+    }
+}
+
+export const GetSearchHistory = async (Email) => {
+    try {
+        const response = await apiClient.get(`/api/history/${Email}`);
+
+        return response.data;
+    }
+    catch (error) {
+        throw error.response?.data || error.message;
+    }
+}
+
+export const DeleteHistory = async (Elements) => {
+    try {
+        const response = await apiClient.put("/api/history", {
+            Elements
+        });
+
+        return response.data;
+    }
+    catch (error) {
         throw error.response?.data || error.message;
     }
 }
