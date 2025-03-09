edfe4b349851049f4a6d077bfd2608519e638746
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2025-03-09 17:12:28 -0600

    Add search history retrieval and deletion endpoints in history controller

--- backEnd/src/controllers/historyController.js
+++ backEnd/src/controllers/historyController.js
@@ -25,3 +25,52 @@ export const InsertSearch = async (req,res) => {
         console.log(error);
     }
 }
+
+export const GetSearchHistory = async (req,res) => {
+    try{
+        const {Email} = req.params;
+
+        const HistoryFound = await SearchHistory.findAll({
+            where: {Email: Email,Search_Status:true}
+        });
+
+        const payload = {
+            status: HistoryFound ? true : false,
+            data: HistoryFound,
+            message: HistoryFound ? "History found successfully!" : "No history was found",
+        }
+
+        res.json(payload);
+    }
+    catch(error)
+    {
+        res.status(500).json(error);
+        console.log(error);
+    }
+}
+
+export const DeleteHistory = async (req,res) => {
+    try{
+        const {Elements} = req.body;
+
+        for (const element of Elements)
+        {
+            await SearchHistory.update({
+                Search_Status: false,
+            },
+            {where:{HistoryId: element}});
+        }
+
+        const payload ={
+            status: true,
+            message:"Elements deleted from history successfully!",
+        }
+
+        res.json(payload);
+    }
+    catch(error)
+    {
+        res.status(500).json(error);
+        console.log(error);
+    }
+}
--- backEnd/src/routes/historyRoutes.js
+++ backEnd/src/routes/historyRoutes.js
@@ -1,8 +1,10 @@
-import { InsertSearch } from "../controllers/historyController.js";
+import { DeleteHistory, GetSearchHistory, InsertSearch } from "../controllers/historyController.js";
 import e from "express";
 
 const router = e.Router();
 
 router.post("/", InsertSearch);
+router.get("/:Email",GetSearchHistory);
+router.put("/", DeleteHistory);
 
 export default router;
