5f674b63946fa92b67c0c1bc92d4e0e9a43dc609
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-15 17:52:45 -0600

    Modified NotFfound page desing

--- frontEnd/src/pages/NotFound.jsx
+++ frontEnd/src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import Logo from '../assets/Logo.png';
+import {Link} from "react-router-dom";
+
 function NotFound()
 {
     return(
-        <div>
-            404 Page Not Found
+        <div className="flex justify-center m-100 items-center">
+            <img src={Logo} alt="LogoName" className='w-1/10'/>
+            <h1 className='text-xl'>404 Oops!! Page Not Found :( <Link to={"/Home"} className='underline'>Go back home.</Link></h1>
         </div>
     )
 }
