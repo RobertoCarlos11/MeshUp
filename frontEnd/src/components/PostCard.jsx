7f42a94459ac82523d61f6ef7f62c87412d369c3
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-15 09:05:58 -0600

    Merge corrections

--- frontEnd/src/components/PostCard.jsx
+++ frontEnd/src/components/PostCard.jsx
@@ -2,7 +2,6 @@ import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-
 import Rating from './Rating';
 import Scene from './Three/Scene';
 import Button_Style from './Button_Style';
@@ -253,66 +252,9 @@ function PostCard({ Post }) {
                             <BookmarkBorderIcon className='cursor-pointer'/>
                             <p className="text-base m-1">{saves}</p>
                         </div>
-                        
-                        <Popover className="relative">
-                            <PopoverButton className='flex items-center my-2'>
-                                <MoreVertOutlinedIcon className='cursor-pointer'/>
-                            </PopoverButton>
-
-                            <PopoverPanel className='absolute left-0 w-50 h-auto bg-[var(--background-color)] shadow-sm border-2 border-solid border-[var(--primary-color)] rounded-sm p-2 z-50 text-xs text-comp-1 flex flex-col space-y-2'>
-                                <div className='border-b-1 border-solid border-[var(--primary-color)]'>
-                                    <AddOutlinedIcon/>
-                                    <button className='cursor-pointer' onClick={handleOpenCollection}>Add to New Collection</button>
-                                </div>
-
-                                <Modal open= {openCollection} onClose= {handleCloseCollection} className='flex items-center justify-center'>
-                                    <div className="flex items-center justify-center w-1/3 h-1/2">
-                                        <div className="p-8 bg-color rounded shadow-lg">
-                                            <header className='border-b-2 border-colid border-[var(--primary-color)] p-2'>
-                                                <h2 className='text-center text-lg font-bold'>New Collection</h2>
-                                            </header>
-                                            <form className='flex flex-col space-x-6 py-8 mx-4 text-center'>
-                                                <span className='text-sm font-bold mb-4'>Name your new collection!</span>
-                                                <input onChange={e => setCollectionName(e.currentTarget.value)} type="text" name="NewCollectionName" placeholder="New Collection Name" className='text-comp-1 w-full p-2 py-1 border-b-1 border-solid border-[var(--primary-color)]'/>
-                                            </form>
-                                            <footer className='text-center border-t-2 border-colid border-[var(--primary-color)] p-2'>
-                                                <Button_Style className='text-sm m-2 p-3 py-1' onClick={createCollection}>Save</Button_Style>
-                                                <Button_Style className='text-sm m-2 p-3 py-1' onClick={handleCloseCollection}>Cancel</Button_Style>
-                                            </footer>
-                                        </div>
-                                    </div>
-                                </Modal>
-
-                                {user ? (
-                                <Popover className='relative'>
-                                    <PopoverButton className='flex items-center my-2'>
-                                        <div className='border-b-1 border-solid border-[var(--primary-color)]'>
-                                            <BookmarkBorderIcon/>
-                                            <button className='cursor-pointer' onClick={getUserCollections}>Add to Existing Collection</button>
-                                        </div>
-                                    </PopoverButton>
-                                    <PopoverPanel className='absolute left-50 w-50 h-auto bg-[var(--background-color)] shadow-sm border-2 border-solid border-[var(--primary-color)] rounded-sm p-2 z-50 text-xs text-comp-1 flex flex-col space-y-2'>
-                                    {userCollections?.length > 0 ? ( userCollections.map(collection => (
-                                        <button 
-                                            key={collection.CollectionId} 
-                                            onClick={() => {
-                                                setCollectionId(collection.CollectionId);
-                                                addToCollection(collection.CollectionId);
-                                            }} 
-                                            className="flex justify-start border-b-1 border-solid border-[var(--primary-color)] cursor-pointer">
-                                            {collection.Collection_Name}
-                                        </button>
-                                    ))) : (<span className='flex justify-start border-b-1 border-solid border-[var(--primary-color)] cursor-default'>No Collections yet</span>)}
-                                    </PopoverPanel>
-                                </Popover>
-                                ) : (
-                                    <div className='border-b-1 border-solid border-[var(--primary-color)]'>
-                                        <BookmarkBorderIcon />
-                                        <button className='cursor-pointer' onClick={handleNoSession}>Add to Existing Collection</button>
-                                    </div>
-                                )}
-                            </PopoverPanel>
-                        </Popover>
+                        <AddCollection userLoggedIn={user} Post={Post} > 
+                            <MoreVertOutlinedIcon className='cursor-pointer' />
+                        </AddCollection>
                     </div>
                 </div>
             </div>
