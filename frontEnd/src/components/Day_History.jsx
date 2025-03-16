4208c24c34ccc3e1da0bddae64a67dbb90f17c60
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-15 19:19:59 -0600

    Modified date display

--- frontEnd/src/components/Day_History.jsx
+++ frontEnd/src/components/Day_History.jsx
@@ -1,6 +1,21 @@
 import Search_Element from "./Search_Element";
+import { useState, useEffect } from "react";
 
-function Day_History({Date,History, HistorySelected, Items, HandleWord}){
+function Day_History({SearchDate,History, HistorySelected, Items, HandleWord}){
+
+    const [date, setDate] = useState();
+
+    useEffect(() => {
+        const [day, month, year] = SearchDate.split('/'); 
+        const dateObj = new Date(year, month - 1, day); 
+        const finalDate = new Intl.DateTimeFormat("en-GB", { 
+            day: "numeric", 
+            month: "long", 
+            year: "numeric" 
+        }).format(dateObj);
+
+        setDate(finalDate);
+    }, [SearchDate]);
 
     const handleWord = (value) => {
         HandleWord(value);
@@ -8,10 +23,11 @@ function Day_History({Date,History, HistorySelected, Items, HandleWord}){
     const ElementSelected = (index) => {
         HistorySelected(index);
     }
+
     return(
         <>
         <div className="bg-[var(--transparent-color)] rounded-sm p-8 mt-8 mb-8">
-            <span className="text-xl font-semibold">{Date}</span>
+            <span className="text-xl font-semibold">{date}</span>
 
             <div className="flex flex-col m-5">
                 {History.map(item => 
