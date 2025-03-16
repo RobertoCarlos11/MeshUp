729e70e632d3b26042cb7107c018c78dc623ea1a
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-03-15 21:24:49 -0600

    Dummies for categories and users

---
+++ DB/INSERTS.sql
@@ -0,0 +1,22 @@
+INSERT INTO user (Email, Username, Pass, Birthdate)
+VALUES 
+    ('annie@gmail.com', 'Luvannie', 'Annie123$', '2004-05-27'),
+    ('carlitos@gmail.com', 'Carlitos', 'Carlitos123$', '2004-04-30'),
+    ('chemamix@gmail.com', 'Chemamix', 'Chemamix123$', '2004-03-05'),
+    ('rosie@gmail.com', 'RosieBonita', 'Rosie123$', '2003-08-04'),
+    ('dobeto@gmail.com', 'Dobeto', 'Dobeto123$', '2004-11-30'),
+    ('leo@gmail.com', 'Leon', 'Leo123$', '2004-04-03');
+
+INSERT INTO category (Category_Name, Category_Description) 
+VALUES 
+('Technology', 'All about the latest tech trends and gadgets.'),
+('Science', 'Discussions on scientific discoveries and research.'),
+('Health', 'Topics related to fitness, nutrition, and wellness.'),
+('Education', 'Learning resources and academic discussions.'),
+('Entertainment', 'Movies, music, and pop culture news.'),
+('Sports', 'News and discussions about different sports.'),
+('Business', 'Market trends, startups, and entrepreneurship.'),
+('Lifestyle', 'Fashion, travel, and daily life tips.'),
+('Gaming', 'Video games, esports, and gaming culture.'),
+('Art', 'Painting, photography, and creative arts.');
+
